0932bea6cb0dbe0e7e08855b07397fcef6cd8cff
Sergio Quijano <sergiquijano@gmail.com>
2017-11-07 17:55:13 +0100

    I change the "Readme.MD" and I upload an algorithm for caculating number Pi

---
+++ C++/Number Pi using series/calculatePi.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/*
+Author:
+    Sergio Quijano Rey
+    sergiquijano@gmail.com
+Description:
+    The program calculates the number Pi using the wide known Basilea's serie
+Version:
+    v1.0
+    7th November 2017
+*/
+
+long double calculatePi(int limit){
+    double sum = 0;
+    long double pi;
+
+    for(int i = 1; i < limit; i++){
+        sum = sum + (1.0 / (i * i));
+    }
+
+    pi = sqrt(6.0 * sum);
+    return pi;
+}
+
+int main(){
+    int top;
+
+    cout << "Introduce the limit of the calculations: ";
+    cin >> top;
+
+    long double PI = calculatePi(top);
+    cout << "The value of Pi calculated through " << top << " iterations is: " << PI << endl;
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Small projects
 I upload small programs that I write and I find interesting to share.
 Most of them are algorithmits already implemented, by here is my version of that implementations (probably worst than existing ones)
-Sorry if you find typos or bad grammar, I am not a native english speaker, hope you understand
+
+*Sorry if you find typos or bad grammar, I am not a native english speaker, hope you understand*
