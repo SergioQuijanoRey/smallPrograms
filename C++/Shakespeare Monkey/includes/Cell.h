be6a427c29248049df6356216cc13bb704d12551
Sergio Quijano <sergiquijano@gmail.com>
2018-03-13 19:26:30 +0100

    Cambios al proyecto "Shakespeare Monkey"

--- C++/Shakespeare Monkey/includes/Cell.h
+++ C++/Shakespeare Monkey/includes/Cell.h
@@ -1,7 +1,20 @@
 #ifndef _CELL_H_
 #define _CELL_H_
+
+#include <string>
+
 class Cell{
 public:
+    //Constructors
+    Cell();
+    Cell(std::string _target, int _mutationRate, bool randomize);
+
+    //Observers
+
+    //Modifiers
+    void calculateFitness();
+    void randomDNA();
+    
 private:
     int size;
     int mutationRate;
--- C++/Shakespeare Monkey/includes/Population.h
+++ C++/Shakespeare Monkey/includes/Population.h
@@ -1,11 +1,25 @@
 #ifndef _POPULATION_H_
 #define _POPULATION_H_
 
+#include <string>
+#include <vector>
+#include "Cell.h"
+
 class Population{
 public:
+    //Constructors
+    Population();
+    Population(int _size, int _mutationRate, std::string _target);
+
+    //Observers
+    void show() const;
+
+    //Modifiers
+
 private:
     int size;
     int mutationRate;
+    std::vector<Cell> pop;
     std::string target;
 };
 
